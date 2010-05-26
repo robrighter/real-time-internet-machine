066a9261029a43c2ece6262c8bb82286579041f6
Robert Righter <robrighter@gmail.com>
2010-05-25 23:36:30 -0400

    Added very dumb and simple load balancer

---
+++ loadbalancer.js
@@ -0,0 +1,21 @@
+var sys = require('sys');
+
+exports.LoadBalancer = function(workerslist){
+    var workers = workerslist;
+    var next;
+    
+    this.getNextWorkerServer = function(){
+        next = workers.shift();
+        workers.push(next);
+        sys.inspect(workers);
+        return next;
+    }
+}
+
+
+//TEST
+// var lb = new LoadBalancer(['one','two','three','four','five']);
+// for(var i = 0; i< 20; i++)
+//  sys.puts(lb.getNextWorkerServer());
+  
+
