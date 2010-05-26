d165d83d471c5b05822bc2a6a450bf7a6fae354c
Robert Righter <robrighter@gmail.com>
2010-05-26 00:12:04 -0400

    setup redirectors on dispatcher

--- dispatcher.js
+++ dispatcher.js
@@ -4,7 +4,7 @@ var querystring = require('querystring');
 var uuid = require('./lib/uuid');
 var server = noderouter.getServer();
 var url = require('url');
-var LoadBalancer = new require('loadbalancer').LoadBalancer;
+var LoadBalancer = new require('./loadbalancer').LoadBalancer;
 
 var validhash = '.[0-9A-Za-z_\-]*';
 var loadbalancer = new LoadBalancer([
@@ -17,14 +17,19 @@ var loadbalancer = new LoadBalancer([
 
 // STORAGE /////////////////////////////////////////////////////////////////////////////////////////
 var feeds = {
-    a03902-834938-akejhne : "http://localhost:8081",
-    b03902-834938-akejhne : "http://localhost:8081",
-    c03902-834938-akejhne : "http://localhost:8081",
-    d03902-834938-akejhne : "http://localhost:8081",
+    "a03902-834938-akejhne" : "http://localhost:8081",
+    "b03902-834938-akejhne" : "http://localhost:8081",
+    "c03902-834938-akejhne" : "http://localhost:8081",
+    "d03902-834938-akejhne" : "http://localhost:8081",
 };
 
 var lookupWorkerURLByHash = function(hash){
-    return feeds[hash];
+    if(feeds.hasOwnProperty(hash)){
+        return feeds[hash];
+    }
+    else{
+        return "";
+    }
 };
 
 
@@ -39,26 +44,37 @@ var bootstrap = function(){
 // WORKER CONTROL /////////////////////////////////////////////////////////////////////////////////
 var addFeedToClient = function(url, hash){
     //TODO: Call out to the client and setup the feed
+    
     feeds[hash] = url;
+    return {'status':'success', 'feed':hash}
 };
 
 
 // ROUTING  ////////////////////////////////////////////////////////////////////////////////////////
 //Create a feed
-server.get("createfeed", function (req, res, match) {
-   addFeedToClient(loadbalancer.getNextWorkerServer(), uuid.getUuid()); 
+server.get("/createfeed", function (req, res, match) {
+   return addFeedToClient(loadbalancer.getNextWorkerServer(), uuid.getUuid()); 
 });
 
 //Dispatch long polling to the correct worker
-server.get(new RegExp("^/latest/("+validhash+")$"), function (req, res, match) {
-      //TODO:need to check and make sure the feed exists
-      res.redirect( lookupWorkerURLByHash(match)+"/latest/"+match);
+server.get(new RegExp("^/latest/("+validhash+")$"), function (req, res, hash) {
+      if(url = lookupWorkerURLByHash(hash))
+          res.redirect( url +"/latest/"+hash);
+      else
+          return {status:'error', message:'invalid feed identifier'};
 });
 
 //Dispatch insert to the correct worker
-server.post(new RegExp("^/insert/("+validhash+")$"), function(req,res,match){
-    //TODO:need to check and make sure the feed exists
-    res.redirect(  lookupWorkerURLByHash(match)+"/insert/"+match);
+server.post(new RegExp("^/insert/("+validhash+")$"), function(req,res,hash){
+    if(url = lookupWorkerURLByHash(hash)){
+        res.redirect(  url +"/insert/"+hash);
+        res.end();
+    }
+    else{
+        res.sendHeader(200,{"Content-Type": "application/json"});
+        res.write("{'status':'error', 'message':'invalid feed identifier'}");
+        res.end();
+    }
 }, "form-url-encode");
 
 
